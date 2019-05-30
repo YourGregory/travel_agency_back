9736bd6ec100a34d4402128490ec49852195ad01
VadimSymotiuk <vadim.symotiuk@inventorsoft.co>
2019-05-30 22:10:22 +0300

    The end 8

--- src/main/java/com/kursova/travel/repository/CampaignRepository.java
+++ src/main/java/com/kursova/travel/repository/CampaignRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
 
-//    @Query(value = "SELECT tr from Section s inner join s.group")
-//    List<Training> getAllTraining(Long sectionId);
+    @Query(value = "SELECT tr from Section s inner join s.scheduler sc inner join sc.training tr inner join tr.group g inner join  g.tourists inner join Campaign cm on g.tourists = cm.tourists where s.id = ?1")
+    List<Training> getAllTraining(Long sectionId);
 
 }
